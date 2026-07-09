86f385982dfb3f098f201880e1a004a45b7cde51
wwg98 <wwwg98@gmail.com>
2026-07-09 11:06:00 +0900

    Refactor App component and enhance Auth form with Material-UI components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import Router from "./Router";
+import { useState } from "react";
+import { authService } from "../firebase";
+import { Container } from "@mui/material";
 import "./App.css";
 
 function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
-      <h1>ESTFE-X</h1>
-      <Router />
+      <Container>
+        <h1>ESTFE-X</h1>
+        <Router isLoggedIn={isLoggedIn} />
+      </Container>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
-import { firebase } from "./firebase.js";
+// import { firebase } from "./firebase.js";
 import { BrowserRouter } from "react-router";
-console.log(firebase);
+// console.log(firebase);
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
