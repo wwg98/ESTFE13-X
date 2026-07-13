e0db2b4ab10f50ff0635d5d66f20607ce70b1bc1
wwg98 <wwwg98@gmail.com>
2026-07-13 10:35:39 +0900

    Implement comments retrieval and display in Home component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,42 @@
-import { Box, Typography, TextField, Button, Divider } from "@mui/material";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  useScrollTrigger,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { collection, addDoc, serverTimestamp, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  /*
+  useEffect로 데이터를 조회 결과를 변수명 comments할당
+  */
+  const getComments = async () => {
+    const q = query(collection(db, "comments"));
+
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot);
+    const commentsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setComments(commentsArray);
+  };
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  console.log(comments);
 
   const handleChange = e => {
     setComment(e.target.value);
   };
-
   const onSubmit = async e => {
     e.preventDefault();
     try {
@@ -23,11 +50,10 @@ function Home() {
       console.error("글 추가시 에러가 발생했습니다.", e);
     }
   };
-
   return (
     <>
       <Typography variant="h2" component="h2">
-        Home
+        Home{" "}
       </Typography>
 
       <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
@@ -48,6 +74,13 @@ function Home() {
         </Button>
       </Box>
       <Divider sx={{ my: 3 }} />
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {comments.map(item => (
+          <ListItem key={item.id} alignItems="flex-start" divider>
+            <ListItemText primary={item.comment} secondary={item.date.toDate().toLocaleString()} />
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
